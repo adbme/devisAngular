5613092cf0a960e9c394439142e7effef3993bc7
ad.bme <adil.bouamrioune@gmail.com>
2023-07-05 13:46:07 +0200

    all commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AppTest} from "./apptest.component";
+
+import { DevisComponent } from './devis/devis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTest,
+    DevisComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/apptest.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-apptest',
+  template: `<div>Hello bro</div>`,
+  styleUrls: []
+})
+export class AppTest{
+
+}
